124a3e77f94f388eacfac9d0f0b89ea09a093b77
Lucas Eduardo <lucas59356@gmail.com>
2020-05-25 12:37:30 -0300

    add chdir to repo before everything

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"github.com/joho/godotenv"
 )
 
 var DotCtlRepository = NewDotctlRepo()
 
 func main() {
+	err := os.Chdir(DotCtlRepository.basepath)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err.Error())
+		return
+	}
 	godotenv.Load() // if .env dont exists, no problem
-	err := RootCMD.Execute()
+	err = RootCMD.Execute()
 	if err != nil {
 		fmt.Printf("ERROR: %s\n", err.Error())
 	}
